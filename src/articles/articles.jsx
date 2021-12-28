be47436a29d002b2cd80c3ec84bfc0c0643614e7
Dominick Agnello <dagnello@asu.edu>
2021-12-27 18:51:07 -0700

    change profile icon size, change mobile top padding between content and menu, change link style to be consistent (red)

--- src/articles/articles.jsx
+++ src/articles/articles.jsx
@@ -8,8 +8,8 @@ const Articles = () => {
 		<div className="articles-container">
 			<ul className="articles-list">
 			<h1>2021</h1>
-				<li><Link to="/blog/how_the_blog_works">2021 Dec 01 &ndash; How the Blog Works</Link></li>
-				<li><Link to="/blog/revamping_my_website_and_making_a_blog">2021 Nov 21 &ndash; Revamping My Website and Making a Blog</Link></li>
+				<li><Link to="/blog/how_the_blog_works">2021 Dec 01 &ndash; <span>How the Blog Works</span></Link></li>
+				<li><Link to="/blog/revamping_my_website_and_making_a_blog">2021 Nov 21 &ndash; <span>Revamping My Website and Making a Blog</span></Link></li>
 			</ul>
 		</div>
 	)
