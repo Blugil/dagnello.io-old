6af74387e71c23aeb215c6b78c30bee7d867d493
Dominick Agnello <dagnello@asu.edu>
2023-01-23 00:21:23 -0700

    restructure css by addng variables to control text and background color

--- src/components/recent_articles/recent_articles.jsx
+++ src/components/recent_articles/recent_articles.jsx
@@ -8,6 +8,7 @@ const recent = () => {
     <div className='recent-articles-container'>
         <h1>Recent Articles</h1>
         <ul>
+          <li><Link to="/blog/my_solution_to_new_years_resolutions">2023 Jan 19 &ndash; <span>My Solution to New Years Resolutions</span></Link></li> 
           <li><Link to="/blog/how_the_blog_works">2021 Dec 01 &ndash; <span>How the Blog Works</span></Link></li>
           <li><Link to="/blog/revamping_my_website_and_making_a_blog">2021 Nov 21 &ndash; <span>Revamping My Website and Making a Blog</span></Link></li>
         </ul>
